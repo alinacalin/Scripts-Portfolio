49d49cb7034ebe7e58abc0b89c5e9efe6d150168
alinacalin <16622725+alinacalin@users.noreply.github.com>
2022-05-06 14:19:56 +0300

    Create count_lines_of_c_code.sh

---
+++ Bash/count_lines_of_c_code.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#Cound the number of non-empty lines of C code in the directory provided as cmd line arg.
+
+if [ $# -eq 0 ]
+then
+    echo "Provide an argument."
+    exit 1
+fi
+
+if [ ! -d $1 ]
+then
+    echo "The argument must be a valid directory."
+    exit 1
+fi
+
+DIR=$1
+S=0
+
+for F in `find $DIR -type f -name "*.c"`
+do
+        echo $F
+        N=`grep -E -v -c "^$" $F`
+        echo $N
+        S=`expr  $S + $N`
+done
+
+echo $S
