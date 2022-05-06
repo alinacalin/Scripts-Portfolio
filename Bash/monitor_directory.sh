0ee49cdc5a3bc139b72ed50176cbb528b88976a5
alinacalin <16622725+alinacalin@users.noreply.github.com>
2022-05-06 16:43:44 +0300

    Create monitor_directory.sh

---
+++ Bash/monitor_directory.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+#Monitor the state of a directory, notifying when something is changed. 
+
+D=$1
+
+if [ -z "$D" ]; then
+        echo "ERROR: No dir provided for monitoring" >&2
+        exit 1
+fi
+
+if [ ! -d "$D" ]; then
+        echo "ERROR:Dir $D does not exits" >&2
+        exit 1
+fi
+
+STATE=""
+while true; do
+        S=""
+        for P in `find $D`; do
+                if [ -f $P ]; then
+                        LS=`ls -l $P | sha1sum`
+                        CONTENT=`sha1sum $P`
+                elif [ -d $P ]; then
+                        LS=`ls -l -d $P | sha1sum`
+                        CONTENT=`ls -l $P | sha1sum`
+                fi
+                S="$S\n$LS $CONTENT"
+
+        done
+
+        if [ -n "$STATE" ] && [ "$S" != "$STATE" ]; then
+                echo "Directory state changed."
+        fi
+        STATE=$S;
+        sleep 1
+done
