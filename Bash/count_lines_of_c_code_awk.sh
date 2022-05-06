a6e01f7b3c931f76fd83bfcc7f26ee540967939a
alinacalin <16622725+alinacalin@users.noreply.github.com>
2022-05-06 14:25:26 +0300

    Update count_lines_of_c_code_awk.sh

--- Bash/count_lines_of_c_code_awk.sh
+++ Bash/count_lines_of_c_code_awk.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+if [ $# -eq 0 ] || [ ! -d $1 ] || [ ! -r $1 ]
+then
+    echo "Usage: run with an argument that is a valid directory with read permissions."
+    exit 1
+fi
+
 awk -f scr.awk $1/*.c
 
-#--------------acr.awk---------------
+#--------------scr.awk---------------
 #  #!/bin/awk -f
 #  BEGIN { s=0 }
 #  /[^ \t]/  { s+=1 }
