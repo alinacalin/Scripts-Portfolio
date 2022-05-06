88d6e8cf32581d680c54f013158af646bf01b2b6
alinacalin <16622725+alinacalin@users.noreply.github.com>
2022-05-06 17:44:58 +0300

    Create the_matrix.py

---
+++ Python/the_matrix.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+import math
+import os
+import re
+import sys
+
+
+# read dimensions of the matrix
+first_multiple_input = input().rstrip().split()
+n = int(first_multiple_input[0])
+m = int(first_multiple_input[1])
+
+# read the matrix
+matrix = []
+for _ in range(n):
+    matrix_item = input()
+    matrix.append(matrix_item)
+
+# chage lines with columns in the matrix 
+res = [line[i] for i in range(m) for line in matrix]
+res = ''.join(res)
+# this is the equivalent of:
+#res=""
+#for i in range(m):
+#    for j in range(n):
+#        res+=str(matrix[j][i])
+ 
+# decode string by replacing special characters and/or space between alphanumeric words with only one space.
+x = re.sub(r"([a-zA-Z0-9]+)[!@#$%&\s]+(?=[a-zA-Z0-9]+)", r"\1 ", res)
+print(x)
