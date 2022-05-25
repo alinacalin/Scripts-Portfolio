2915507ae2c76b51a27d41927ebdd43c855a20fa
alinacalin <16622725+alinacalin@users.noreply.github.com>
2022-05-25 17:36:23 +0300

    Create square_root.go

---
+++ Go/square_root.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func Sqrt(x float64) float64 {
+	z := 1.0
+	z1 := 0.0
+	for math.Abs(z-z1) > 0.00001 {
+		z1 = z
+		z -= (z*z - x) / (2 * z)
+		fmt.Println(z)
+	}
+	return z
+}
+
+func main() {
+	fmt.Println("The square root of 66 is ", Sqrt(66))
+}
